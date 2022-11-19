d4b02961de54b6a5ae79846b15a5a555e868863a
kalerukoushik <kalerukoushik@gmail.com>
2022-11-19 20:41:31 +0530

    Converted function based views to Class based

--- polls/old_views.py
+++ polls/old_views.py
@@ -9,21 +9,29 @@ from .models import Question, Choice
 
 # Create your views here.
 
-class IndexView(generic.ListView):
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_question_list'
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return render(request, 'polls/index.html', context)
 
-	def get_queryset(self):
-		''' Return the recent 5 publications '''
-		return Question.objects.order_by('-pub_date')[:5]
+def detail(request, question_id):
+	# try:
+	# 	question = Question.objects.get(pk=question_id)
+	# except Question.DoesNotExist:
+	# 	raise Http404("Question doesn't exists!")
 
-class DetailView(generic.DetailView):
-	model = Question
-	template_name = 'polls/detail.html'
+	question = get_object_or_404(Question, pk=question_id)
+	context = {
+		'question': question,
+	}
+	return render(request, 'polls/detail.html', context)
 
-class ResultsView(generic.DeleteView):
-	model = Question
-	template_name = 'polls/results.html'
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
